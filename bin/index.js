678c3e127455d2257186f3643ef8a862189660c0
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2019-08-22 11:15:50 -0300

    validacao -h, -v, --help, --version

--- bin/index.js
+++ bin/index.js
@@ -4,10 +4,9 @@ const minimist = require('minimist')
 const fs = require('fs')
 const path = require('path')
 
-
 const args = minimist(process.argv.slice(2))
 let cmd = (args._[0] || 'help').toLowerCase()
-const filepath = path.resolve(__dirname, `../bin/cmds/${cmd}.js`)
+const filepath = getFilePath(cmd)
 
 if (fs.existsSync(filepath)) {
   args.figlet = true
@@ -16,4 +15,18 @@ if (fs.existsSync(filepath)) {
   console.log(chalk.yellow(`
   ${cmd}" is not a valid command!
   `))
+}
+
+function helpCmd(cmd){
+  return (cmd == '--help'|| cmd == '-h'|| cmd == 'help') ? 'help': cmd
+}
+
+function versionCmd(cmd){
+  return (cmd == '--version'|| cmd == '-v'|| cmd == 'version') ? 'version': cmd
+}
+
+function getFilePath(cmd){
+  cmd = helpCmd(cmd)
+  cmd = versionCmd(cmd)
+  return path.resolve(__dirname, `../bin/cmds/${cmd}.js`)
 }
